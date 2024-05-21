91136ff2dcdc4453c7bf2e867f331a3e895c29ce
Anthony Martin <38542602+anthony-c-martin@users.noreply.github.com>
2024-05-21 10:27:10 -0400

    Simplify publish script

--- src/Bicep.Extension.Kubernetes/publish.sh
+++ src/Bicep.Extension.Kubernetes/publish.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 set -e
 
-usage="Usage: ./publish.sh <repo_name> <version>"
-repo_name=${1:?"Missing repo_name. ${usage}"}
-version=${2:?"Missing version. ${usage}"}
+usage="Usage: ./publish.sh <target>"
+target=${1:?"Missing target. ${usage}"}
 
 root=$(dirname ${BASH_SOURCE[0]})
 ext_name="bicep-ext-kubernetes"
-repo_path="providers/kubernetes"
 
 # build various flavors
 dotnet publish --configuration release --self-contained true -r osx-arm64 $root
@@ -20,5 +18,5 @@ dotnet publish --configuration release --self-contained true -r win-x64 $root
   --ext-osx-arm64 "$root/bin/release/net8.0/osx-arm64/publish/$ext_name" \
   --ext-linux-x64 "$root/bin/release/net8.0/linux-x64/publish/$ext_name" \
   --ext-win-x64 "$root/bin/release/net8.0/win-x64/publish/$ext_name.exe" \
-  --target br:$repo_name.azurecr.io/$repo_path:$version \
+  --target "$target" \
   --force
